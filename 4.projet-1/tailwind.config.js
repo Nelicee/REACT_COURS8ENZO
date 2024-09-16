f4f21a09c98aafd24aa75ef8345b62dbe68198dd
Céline DEVOS <140008750+Nelicee@users.noreply.github.com>
2024-09-16 20:27:16 +0200

    exo todo-list

---
+++ 4.projet-1/tailwind.config.js
@@ -0,0 +1,16 @@
+import forms from '@tailwindcss/forms';
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+
+  plugins: [
+   forms
+  ],
+}
+
+
