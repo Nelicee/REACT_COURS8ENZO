ba81562dda91c1c4b1f328bf6b2d15d9dbb74139
Céline DEVOS <140008750+Nelicee@users.noreply.github.com>
2024-09-16 09:47:05 +0200

    Les bases de React fin

--- 3.Les-bases-de-React/src/lessons/13.useRef/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/13.useRef/Container.jsx
@@ -6,20 +6,23 @@ import { useRef, useState } from "react";
 export default function Container() {
   const [state, setState] = useState(0);
   const countRef = useRef(0);
-  console.log(countRef.current);
 
-  function handleCountRef(){
-    countRef.current++
-    console.log(countRef.current)
+
+  function handleCountRef() {
+    countRef.current++;
+    console.log(txtRef.current);
   }
-  console.log("Mise à jour")
+  const txtRef = useRef();
+  console.log(txtRef);
+
   return (
     <div>
       <h1>Le hook useRef</h1>
       <p>Valeur du state {state}</p>
       <p>Valeur de la réf {countRef.current}</p>
       <button onClick={handleCountRef}>Incrémenter la ref</button>
-      <button onClick={() => setState(state +1 )}>Incrémenter la ref</button>
+      <button onClick={() => setState(state + 1)}>Incrémenter le state</button>
+      <p ref={txtRef}>Lorem ipsum dolor sit amet.</p>
     </div>
   );
 }
--- 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
+++ 3.Les-bases-de-React/src/lessons/14.Controlled-uncontrolled-inputs/Container.jsx
@@ -1,10 +1,33 @@
-import "./Container.css"
+import { useState, useRef } from "react";
+import "./Container.css";
 
 export default function Container() {
+  const [state, setState] = useState("");
 
+  function handleInput(e) {
+    setState(e.target.value);
+  }
+  const emailRef = useRef();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(emailRef.current);
+  }
   return (
     <div>
-      <h1>Composants "controlled" et "uncontrolled"</h1>
+      <h1>Controlled vs Uncontrolled inputs</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="pseudo">Votre pseudo</label>
+        <input onChange={handleInput} value={state} type="text" id="pseudo" />
+
+        {/* {!state && <p>Veuillez remplir cet input</p>} */}
+
+        <label htmlFor="email">Votre email</label>
+        <input ref={emailRef} type="email" id="email" />
+
+        <button>Envoyer</button>
+      </form>
+      <p>Votre pseudo :{state}</p>
     </div>
-  )
+  );
 }
